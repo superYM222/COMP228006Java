37e8b5e118400897521d65cd91586cbfca4c1e8d
Yoon Min <haydenmin219@gmail.com>
2024-09-29 15:48:48 -0400

    Delete week3 directory

--- week3/question3/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println( overloaded.addNums(5,6));
-        System.out.println( overloaded.mulNums(10,2));
-        System.out.println( overloaded.subNums(5,2));
-    }
-}
